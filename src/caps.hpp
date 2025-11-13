d21944e38719b9f1b7b0a59529810fd5ee852f99
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-13 14:02:14 +0330

    Create caps.hpp

---
+++ src/caps.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <vulkan/vulkan.h>
+void override_physical_device_properties2(VkPhysicalDeviceProperties2* p);
+void override_physical_device_features2(VkPhysicalDeviceFeatures2* f);
